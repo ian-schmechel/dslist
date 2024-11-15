76dbd23f632b66c434c64b859b42053dc7536333
Ian Schmechel <ian.schmechelads@gmail.com>
2024-11-15 14:32:09 -0300

    GameMinDTO, GameRepository, GameService, GameController

---
+++ src/main/java/com/ian/dslist/controllers/GameController.java
@@ -0,0 +1,25 @@
+package com.ian.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ian.dslist.dto.GameMinDTO;
+import com.ian.dslist.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+	@Autowired
+	private GameService gameService;
+	
+	@GetMapping
+	public List<GameMinDTO> findAll(){
+		List<GameMinDTO> result = gameService.findAll();
+		return result;
+	}
+}
