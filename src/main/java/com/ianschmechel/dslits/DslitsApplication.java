c7bfd2d273f4043625a28e43e92f81e50a50d651
Ian Schmechel <ian.schmechelads@gmail.com>
2025-05-20 12:15:06 -0300

    Project created

---
+++ src/main/java/com/ianschmechel/dslits/DslitsApplication.java
@@ -0,0 +1,13 @@
+package com.ianschmechel.dslits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslitsApplication.class, args);
+	}
+
+}
